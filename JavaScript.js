e6787fd4c91cc08032de0f80ecd211674888c989
yousukeman3 <yousukeman3@gmail.com>
2016-05-16 16:44:01 +0900

    まずは完成

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,10 @@
-﻿
+﻿var txt1 = document.querySelector("#inputS");
+var txt2 = document.querySelector("#inputF");
 var button =document.querySelector("#button");
 button.addEventListener("click",
-function() {
-    alert("だよ");
-});
+function () {
+    txt1 = Number(txt1.value);
+    txt2 = Number(txt2.value);
+    var txt3 = txt1 + txt2;
+    document.write(txt3 + "");
+});                                                                                                                                                                 
